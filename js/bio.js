eebd5630f647bf9fbd0df78234b4f366d1d1e5ad
Bob Massarczyk <bob@b2m9.com>
2016-03-20 17:11:03 +0000

    Add render for bio info
    - Modify bio.js data
    - Add display function

--- js/bio.js
+++ js/bio.js
@@ -1,15 +1,38 @@
-cv.bio = {
+/* global $, jQuery, HTMLheaderRole, HTMLheaderName, HTMLmobile, HTMLemail, HTMLtwitter, HTMLgithub, HTMLlocation, HTMLbioPic, HTMLwelcomeMsg, HTMLskillsStart, HTMLskills */
+
+var bio = {
     name: "John Doe",
     role: "Professional Adult",
     contacts: {
-        mobile: "",
-        email: "",
-        github: "",
-        twitter: "",
-        location: ""
+        mobile: "+69 1234 5678",
+        email: "hi@jdoe.com",
+        github: "jdoe",
+        twitter: "@jdoe",
+        location: "Singapore"
     },
-    welcomeMessage: "",
-    skills: [],
-    biopic: "",
-    display: function () {}
+    welcomeMessage: "Good day stranger.",
+    skills: ["being professional", "being an adult"],
+    biopic: "http://placehold.it/200x150",
+    display: function () {
+        var d = "%data%"; // handle for placeholder string
+        
+        // add name and role
+        jQuery("#header")
+            .prepend(HTMLheaderName.replace(d, this.name)
+                + HTMLheaderRole.replace(d, this.role))
+            .append(HTMLbioPic.replace(d, this.biopic)
+                + HTMLwelcomeMsg.replace(d, this.welcomeMessage)
+                + HTMLskillsStart);
+                
+        // add skills and fix a CSS bug
+        jQuery("#skills").css("display", "block").append(this.skills.map(function (el) { return HTMLskills.replace(d, el) } ));
+
+        // add contact details
+        jQuery("#topContacts")
+            .append(HTMLmobile.replace(d, this.contacts.mobile)
+                + HTMLemail.replace(d, this.contacts.email)
+                + HTMLtwitter.replace(d, this.contacts.twitter)
+                + HTMLgithub.replace(d, this.contacts.github)
+                + HTMLlocation.replace(d, this.contacts.location));
+    }
 };
